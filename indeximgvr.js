b1954f9bce0ec15d4a3fb0f6e6a8fe14c92eaf26
processnotproduct <tysonkemp@gmail.com>
2017-06-28 23:44:54 -0400

    Update indeximgvr.js

--- indeximgvr.js
+++ indeximgvr.js
@@ -42,7 +42,7 @@ function onLoad() {
   vrView = new VRView.Player('#vrview', {
     width: '100%',
     height: 480,
-    image: 'MUSTANG360.png',
+    image: 'MUSTANG360.jpg',
     is_stereo: false,
     is_autopan_off: true
   });
